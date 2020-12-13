c558ff17e1c10e9b5cdafe623f6cd4482edb9efa
Pavnesh Chaturvedi <2019kuec2037@iiitkota.ac.in>
2020-12-13 19:55:01 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Massaging-Plugin-Backend
 Massaging Plugin Backend
+
+```npm i```
+
+```npm start```
+
+```PORT=3000```
+
+health check
+```/api```
---
+++ models/user.js
@@ -0,0 +1,103 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      maxlength: 50,
+    },
+    instituteName: {
+      type: String,
+    },
+    rollNumber: {
+      type: String,
+    },
+    joiningYear: {
+      type: Number,
+    },
+    email: {
+      institute: {
+        type: String,
+        trim: true,
+        unique: 1,
+      },
+      personal: {
+        type: String,
+        trim: true,
+        unique: 1,
+      },
+    },
+    hash_password: {
+      type: String,
+      minlength: 8,
+    },
+    contact: {
+      type: Number,
+      required: true,
+    },
+    headline: {
+      type: String,
+      required: true,
+    },
+    about: {
+      type: String,
+      required: true,
+    },
+
+    //Base-64 encoded display picture
+    dp: {
+      profile: String,
+      cover: String,
+    },
+
+    //Social Media handles
+    socialHandle: {
+      twitter: {
+        type: String,
+      },
+      facebook: {
+        type: String,
+      },
+      instagram: {
+        type: String,
+      },
+      linkedin: {
+        type: String,
+      },
+      gitHub: {
+        type: String,
+      },
+      discord: {
+        type: String,
+      },
+    },
+
+    socialHandle: {
+      type: [String],
+      default: [],
+    },
+
+    converations: [
+      {
+        conversationID: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Conversations",
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+userSchema.virtual("password").set(function (password) {
+  this.hash_password = bcrypt.hashSync(password, 10);
+});
+
+userSchema.methods = {
+  authenticate: function (password) {
+    return bcrypt.compareSync(password, this.hash_password);
+  },
+};
+
+module.exports = mongoose.model("user", userSchema);
